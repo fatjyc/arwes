06a04f5da8d8350f8a7ed2852c07f8412b555f55
Romel Perez <ronelprhone@gmail.com>
2024-04-18 16:54:40 -0500

    build: clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,12 +2,18 @@
 
 rm -rf ./node_modules/.cache
 rm -rf ./node_modules/@arwes
+
 rm -rf ./packages/*/build
 rm -rf ./packages/*/.turbo
 rm -rf ./packages/*/.tsbuildinfo
+
 rm -rf ./apps/*/build
 rm -rf ./apps/*/.turbo
 rm -rf ./apps/docs/.next
 rm -rf ./apps/docs/out
 rm -rf ./apps/docs/public
+
 rm -rf ./coverage
+rm -rf ./.turbo
+rm -rf ./www
+rm -rf ./www.zip
