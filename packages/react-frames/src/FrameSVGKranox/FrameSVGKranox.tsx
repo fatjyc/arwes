ca3e90fb6976a2afbdb95ab5d3ecd9f7b02b96df
Romel Perez <ronelprhone@gmail.com>
2024-07-18 20:39:33 -0500

    fix(react-frames): FrameSVGKranox elements

--- packages/react-frames/src/FrameSVGKranox/FrameSVGKranox.tsx
+++ packages/react-frames/src/FrameSVGKranox/FrameSVGKranox.tsx
@@ -6,9 +6,10 @@ import { type FrameSVGPath, type FrameSVGStyle, type FrameSVGPathGeneric } from 
 import { type FrameSVGProps, FrameSVG } from '../FrameSVG/index.js'
 
 interface FrameSVGKranoxProps extends FrameSVGProps {
-  squareSize?: number
   padding?: number
   strokeWidth?: number
+  bgStrokeWidth?: number
+  squareSize?: number
   smallLineLength?: number
   largeLineLength?: number
 }
@@ -19,59 +20,75 @@ const toPath = (points: Point[]): FrameSVGPath => points.map((p, i) => [i === 0 
 
 const FrameSVGKranox = memo((props: FrameSVGKranoxProps): ReactElement => {
   const {
+    padding: p = 0,
+    strokeWidth: sw = 2,
+    bgStrokeWidth: bsw = 0,
     squareSize: ss = 16,
-    strokeWidth = 1,
     smallLineLength: sll = 16,
     largeLineLength: lll = 64,
-    padding: p = 0,
     className,
     ...otherProps
   } = props
 
   const paths: FrameSVGPathGeneric[] = useMemo(() => {
-    const so = strokeWidth / 2 // Stroke offset.
+    const so = sw / 2 // Stroke offset.
+    const bso = bsw / 2 // Background stroke offset.
 
     const polylineStyle: FrameSVGStyle = {
       stroke: 'var(--arwes-frames-line-color, currentcolor)',
       strokeLinecap: 'round',
       strokeLinejoin: 'round',
-      strokeWidth: String(strokeWidth),
+      strokeWidth: String(sw),
       fill: 'none',
       filter: 'var(--arwes-frames-line-filter)'
     }
 
     // Left-bottom > left-top > right-top.
-    const leftTopLine: Point[] = [
+    const leftTopBgPolyline: Point[] = [
       // Left-bottom.
-      [so + p + ss * 2, `100% - ${so + p}`],
-      [so + p + ss, `100% - ${so + p + ss}`],
+      [p + bso + ss * 2, `100% - ${p + bso + sw * 2}`],
+      [p + bso + ss, `100% - ${p + bso + sw * 2 + ss}`],
       // Left.
-      [so + p + ss, so + p + lll + ss * 3 + sll],
-      [so + p, so + p + lll + ss * 2 + sll],
-      [so + p, so + p + ss * 2 + sll],
-      [so + p + ss, so + p + sll + ss],
+      [p + bso + ss, p + bso + sw * 2 + lll + ss * 3 + sll],
+      [p + bso, p + bso + sw * 2 + lll + ss * 2 + sll],
+      [p + bso, p + bso + sw * 2 + ss * 2 + sll],
+      [p + bso + ss, p + bso + sw * 2 + sll + ss],
       // Left-top.
-      [so + p + ss, so + p + ss],
-      [so + p + ss * 2, so + p],
+      [p + bso + ss, p + bso + sw + ss],
+      [p + bso + ss * 2 - sw, p + bso + sw * 2],
       // Right-top.
-      [`100% - ${so + p + ss * 2}`, so + p]
+      [`100% - ${p + bso + ss * 2}`, p + bso + sw * 2]
     ]
 
     // Right-top > Right-bottom > Left-bottom.
-    const rightBottomLine: Point[] = [
+    const rightBottomBgPolyline: Point[] = [
       // Right-top.
-      [`100% - ${so + p + ss * 2}`, so + p],
-      [`100% - ${so + p + ss}`, so + p + ss],
+      [`100% - ${p + bso + ss * 2}`, p + bso + sw * 2],
+      [`100% - ${p + bso + ss}`, p + bso + sw * 2 + ss],
       // Right.
-      [`100% - ${so + p + ss}`, `100% - ${so + p + ss * 3 + sll + lll}`],
-      [`100% - ${so + p}`, `100% - ${so + p + ss * 2 + sll + lll}`],
-      [`100% - ${so + p}`, `100% - ${so + p + ss * 2 + sll}`],
-      [`100% - ${so + p + ss}`, `100% - ${so + p + ss + sll}`],
+      [`100% - ${p + bso + ss}`, `100% - ${p + bso + sw * 2 + ss * 3 + sll + lll}`],
+      [`100% - ${p + bso}`, `100% - ${p + bso + sw * 2 + ss * 2 + sll + lll}`],
+      [`100% - ${p + bso}`, `100% - ${p + bso + sw * 2 + ss * 2 + sll}`],
+      [`100% - ${p + bso + ss}`, `100% - ${p + bso + sw * 2 + ss + sll}`],
       // Right-bottom.
-      [`100% - ${so + p + ss}`, `100% - ${so + p + ss}`],
-      [`100% - ${so + p + ss * 2}`, `100% - ${so + p}`],
+      [`100% - ${p + bso + ss}`, `100% - ${p + bso + sw + ss}`],
+      [`100% - ${p + bso + ss * 2 - sw}`, `100% - ${p + bso + sw * 2}`],
       // Left-bottom.
-      [so + p + ss * 2, `100% - ${so + p}`]
+      [bso + p + ss * 2, `100% - ${p + bso + sw * 2}`]
+    ]
+
+    const leftTopLine: Point[] = [
+      [p + so - sw * 2 + ss, p + so + ss + sll],
+      [p + so - sw * 2 + ss, p + so + ss],
+      [p + so - sw * 2 + ss * 2, p + so],
+      [p + so - sw * 2 + ss * 2 + lll, p + so]
+    ]
+
+    const rightBottomLine: Point[] = [
+      [`100% - ${p + so + ss} + ${sw * 2}`, `100% - ${p + so + ss + sll}`],
+      [`100% - ${p + so + ss} + ${sw * 2}`, `100% - ${p + so + ss}`],
+      [`100% - ${p + so + ss * 2} + ${sw * 2}`, `100% - ${p + so}`],
+      [`100% - ${p + so + ss * 2 + lll} + ${sw * 2}`, `100% - ${p + so}`]
     ]
 
     const paths: FrameSVGPathGeneric[] = [
@@ -82,7 +99,7 @@ const FrameSVGKranox = memo((props: FrameSVGKranoxProps): ReactElement => {
           fill: 'var(--arwes-frames-bg-color, currentcolor)',
           filter: 'var(--arwes-frames-bg-filter)'
         },
-        path: toPath(leftTopLine.concat(rightBottomLine))
+        path: toPath(leftTopBgPolyline.concat(rightBottomBgPolyline))
       },
       {
         name: 'line',
@@ -97,7 +114,7 @@ const FrameSVGKranox = memo((props: FrameSVGKranoxProps): ReactElement => {
     ]
 
     return paths
-  }, [sll, lll, ss, strokeWidth, p])
+  }, [p, sw, bsw, sll, lll, ss])
 
   return (
     <FrameSVG
