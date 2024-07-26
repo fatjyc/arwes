b2fcd326631dedc5193621b266e9ba9c9d4c09fd
Romel Perez <ronelprhone@gmail.com>
2024-07-25 23:13:29 -0500

    fix(react-bgs,react-frames): default styles support

--- packages/react-bgs/src/internal/styles.ts
+++ packages/react-bgs/src/internal/styles.ts
@@ -7,8 +7,6 @@ export const positionedStyle: CSSProperties = {
   border: 0,
   margin: 0,
   padding: 0,
-  // In certain browsers, when a canvas has sizes with decimals above the 0.5,
-  // the browser clips the values to the edge. Round down the size so it doesn't happen.
-  width: 'round(down, 100%, 1px)',
-  height: 'round(down, 100%, 1px)'
+  width: '100%',
+  height: '100%'
 }
