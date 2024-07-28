568f45c12c0dc4f81bf7449c854f90eed04a5f63
Romel Perez <ronelprhone@gmail.com>
2024-07-28 18:44:55 -0500

    refactor(animated): easeAmong parameters

--- packages/animated/src/easeAmong/easeAmong.test.ts
+++ packages/animated/src/easeAmong/easeAmong.test.ts
@@ -3,39 +3,42 @@ import { test, expect } from 'vitest'
 import { easeAmong } from './easeAmong'
 
 test('Should return first value if 0', () => {
-  expect(easeAmong(0, [5, 10])).toBe(5)
+  expect(easeAmong([5, 10])(0)).toBe(5)
 })
 
 test('Should return last value if 1', () => {
-  expect(easeAmong(1, [5, 10])).toBe(10)
+  expect(easeAmong([5, 10])(1)).toBe(10)
 })
 
 test('Should return percentage among two values', () => {
-  expect(easeAmong(0, [5, 10])).toBe(5)
-  expect(easeAmong(0.25, [5, 10])).toBe(6.25)
-  expect(easeAmong(0.5, [5, 10])).toBe(7.5)
-  expect(easeAmong(0.75, [5, 10])).toBe(8.75)
-  expect(easeAmong(1, [5, 10])).toBe(10)
+  const easing = easeAmong([5, 10])
+  expect(easing(0)).toBe(5)
+  expect(easing(0.25)).toBe(6.25)
+  expect(easing(0.5)).toBe(7.5)
+  expect(easing(0.75)).toBe(8.75)
+  expect(easing(1)).toBe(10)
 })
 
 test('Should return percentage among ascending and descending values', () => {
-  expect(easeAmong(0, [4, 10, 2])).toBe(4) // 4 + (10 - 4) * 0
-  expect(easeAmong(0.2, [4, 10, 2])).toBe(6.4) // 4 + (10 - 4) * 0.4
-  expect(easeAmong(0.25, [4, 10, 2])).toBe(7) // 4 + (10 - 4) * 0.5
-  expect(easeAmong(0.4, [4, 10, 2])).toBe(8.8) // 4 + (10 - 4) * 0.8
-  expect(easeAmong(0.5, [4, 10, 2])).toBe(10) // 10 - (10 - 2) * 0
-  expect(easeAmong(0.6, [4, 10, 2])).toBe(8.4) // 10 - (10 - 2) * 0.2
-  expect(easeAmong(0.75, [4, 10, 2])).toBe(6) // 10 - (10 - 2) * 0.5
-  expect(easeAmong(0.8, [4, 10, 2])).toBe(5.199999999999999) // 10 - (10 - 2) * 0.6 (JavaScript Glitch)
-  expect(easeAmong(1, [4, 10, 2])).toBe(2) // 10 - (10 - 2) * 1
+  const easing = easeAmong([4, 10, 2])
+  expect(easing(0)).toBe(4) // 4 + (10 - 4) * 0
+  expect(easing(0.2)).toBe(6.4) // 4 + (10 - 4) * 0.4
+  expect(easing(0.25)).toBe(7) // 4 + (10 - 4) * 0.5
+  expect(easing(0.4)).toBe(8.8) // 4 + (10 - 4) * 0.8
+  expect(easing(0.5)).toBe(10) // 10 - (10 - 2) * 0
+  expect(easing(0.6)).toBe(8.4) // 10 - (10 - 2) * 0.2
+  expect(easing(0.75)).toBe(6) // 10 - (10 - 2) * 0.5
+  expect(easing(0.8)).toBe(5.199999999999999) // 10 - (10 - 2) * 0.6 (JavaScript Glitch)
+  expect(easing(1)).toBe(2) // 10 - (10 - 2) * 1
 })
 
 test('Should return percentage among multiple values', () => {
-  expect(easeAmong(0, [1, 2, 3, 4, 5])).toBe(1)
-  expect(easeAmong(0.25, [1, 2, 3, 4, 5])).toBe(2)
-  expect(easeAmong(0.3, [1, 2, 3, 4, 5])).toBe(2.2)
-  expect(easeAmong(0.5, [1, 2, 3, 4, 5])).toBe(3)
-  expect(easeAmong(0.6, [1, 2, 3, 4, 5])).toBe(3.4)
-  expect(easeAmong(0.75, [1, 2, 3, 4, 5])).toBe(4)
-  expect(easeAmong(1, [1, 2, 3, 4, 5])).toBe(5)
+  const easing = easeAmong([1, 2, 3, 4, 5])
+  expect(easing(0)).toBe(1)
+  expect(easing(0.25)).toBe(2)
+  expect(easing(0.3)).toBe(2.2)
+  expect(easing(0.5)).toBe(3)
+  expect(easing(0.6)).toBe(3.4)
+  expect(easing(0.75)).toBe(4)
+  expect(easing(1)).toBe(5)
 })
--- packages/animated/src/easeAmong/easeAmong.ts
+++ packages/animated/src/easeAmong/easeAmong.ts
@@ -1,30 +1,34 @@
-const easeAmong = (progress: number, breakpoints: [number, ...number[]]): number => {
-  if (breakpoints.length === 1) {
-    return progress * breakpoints[0]
-  }
-  if (progress <= 0) {
-    return breakpoints[0]
-  }
-  if (progress >= 1) {
-    return breakpoints[breakpoints.length - 1]
-  }
+import type { EasingFn } from '../easing/index.js'
 
-  const portion = 1 / (breakpoints.length - 1)
-  const portionBase = Math.floor(progress / portion)
-  const portionProgress = progress / portion - portionBase
+const easeAmong =
+  (breakpoints: [number, ...number[]]): EasingFn =>
+  (progress: number): number => {
+    if (breakpoints.length === 1) {
+      return progress * breakpoints[0]
+    }
+    if (progress <= 0) {
+      return breakpoints[0]
+    }
+    if (progress >= 1) {
+      return breakpoints[breakpoints.length - 1]
+    }
 
-  const current = Math.floor(progress * (breakpoints.length - 1))
-  const next = current + 1
-  const from = breakpoints[current]
-  const to = breakpoints[next]
+    const portion = 1 / (breakpoints.length - 1)
+    const portionBase = Math.floor(progress / portion)
+    const portionProgress = progress / portion - portionBase
 
-  if (to > from) {
-    const diff = to - from
-    return from + diff * portionProgress
+    const current = Math.floor(progress * (breakpoints.length - 1))
+    const next = current + 1
+    const from = breakpoints[current]
+    const to = breakpoints[next]
+
+    if (to > from) {
+      const diff = to - from
+      return from + diff * portionProgress
+    }
+
+    const diff = from - to
+    return from - diff * portionProgress
   }
-
-  const diff = from - to
-  return from - diff * portionProgress
-}
 
 export { easeAmong }
