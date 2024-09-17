3d52a9eb03e369df47ebeede1432670b79e2336c
Romel Perez <ronelprhone@gmail.com>
2024-09-16 22:46:27 -0500

    fix(frames): FrameUnderline

--- packages/react-frames/src/FrameUnderline/FrameUnderline.sandbox.tsx
+++ packages/react-frames/src/FrameUnderline/FrameUnderline.sandbox.tsx
@@ -2,19 +2,22 @@
 
 import React, { type ReactElement } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Animator } from '@arwes/react-animator'
 import { FrameUnderline } from '@arwes/react-frames'
 
 const Sandbox = (): ReactElement => {
   return (
-    <div style={{ position: 'relative', width: 300, height: 200 }}>
-      <FrameUnderline
-        style={{
-          // @ts-expect-error css variables
-          '--arwes-frames-bg-color': 'hsl(180, 75%, 10%)',
-          '--arwes-frames-line-color': 'hsl(180, 75%, 50%)'
-        }}
-      />
-    </div>
+    <Animator>
+      <div style={{ position: 'relative', width: 300, height: 200 }}>
+        <FrameUnderline
+          style={{
+            // @ts-expect-error css variables
+            '--arwes-frames-bg-color': 'hsl(180, 75%, 10%)',
+            '--arwes-frames-line-color': 'hsl(180, 75%, 50%)'
+          }}
+        />
+      </div>
+    </Animator>
   )
 }
 
